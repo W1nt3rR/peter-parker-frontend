a3a03e01a19f9f9179bc6a8cd5b25798032034f8
Ramiz Šabović <frostywinterr@gmail.com>
2023-09-10 15:40:32 +0200

    Define environment variable in env.

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,12 @@
+export enum EEnvironment {
+    DEV = "development",
+    PROD = "production",
+    TEST = "test",
+}
+
 const config = {
     apiBaseURL: import.meta.env.VITE_API_BASE_URL as string,
+    environment: import.meta.env.VITE_ENVIRONMENT as EEnvironment,
 };
 
 export default config;
