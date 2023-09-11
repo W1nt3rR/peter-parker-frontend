9791415bf617904697f75299a276eaceaa29728e
Ramiz Šabović <frostywinterr@gmail.com>
2023-09-11 15:40:16 +0200

    Show store.user in home view.

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,16 @@
 <template>
     <div>Home</div>
+    <div>
+        {{ store.user }}
+    </div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+    import { useStore } from "@/stores/store";
+
+    const store = useStore();
+</script>
+
+<style scoped lang="scss">
+    @import "@/sass/includes";
+</style>
