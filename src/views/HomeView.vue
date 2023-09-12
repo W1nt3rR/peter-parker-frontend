4ee3b086e848d3140a6bb64a06d5d7af4d2ff98a
Omer Sadikovic <omer.sadikovic2001@gmail.com>
2023-09-12 17:28:09 +0200

    Add lealfet map zoom to holy enclave

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -3,11 +3,29 @@
     <div>
         {{ store.user }}
     </div>
+    <div style="height: 600px; width: 800px">
+        <LMap
+            ref="map"
+            :use-global-leaflet="false"
+            v-model:zoom="zoom"
+            :center="[43.1421256, 20.5084578]"
+        >
+            <LTileLayer
+                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                layer-type="base"
+                name="OpenStreetMap"
+            ></LTileLayer>
+        </LMap>
+    </div>
 </template>
 
 <script setup lang="ts">
     import { useStore } from "@/stores/store";
+    import "leaflet/dist/leaflet.css";
+    import { LMap, LTileLayer } from "@vue-leaflet/vue-leaflet";
+    import { ref } from "vue";
 
+    const zoom = ref(200);
     const store = useStore();
 </script>
 
