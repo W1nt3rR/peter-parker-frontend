c9afa10fc9a868ef0ebcbf9dbcffdcb8baa7a047
Ramiz Šabović <frostywinterr@gmail.com>
2023-09-12 21:15:24 +0200

    Create initial LayoutComponent. Move map css to style section.

---
+++ src/components/LayoutComponent.vue
@@ -0,0 +1,29 @@
+<template>
+    <div id="layout">
+        <nav>
+            <div>{{ store.user }}</div>
+        </nav>
+        <slot></slot>
+    </div>
+</template>
+
+<script setup lang="ts">
+    import { useStore } from "@/stores/store";
+
+    const store = useStore();
+</script>
+
+<style scoped lang="scss">
+    #layout {
+        display: flex;
+        flex-direction: column;
+        height: 100vh;
+
+        nav {
+            background-color: #333;
+            color: #fff;
+            padding: 1rem;
+            height: 4rem;
+        }
+    }
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,34 +1,45 @@
 <template>
-    <div>Home</div>
-    <div>
-        {{ store.user }}
-    </div>
-    <div style="height: 600px; width: 800px">
-        <LMap
-            ref="map"
-            :use-global-leaflet="false"
-            v-model:zoom="zoom"
-            :center="[43.1421256, 20.5084578]"
-        >
-            <LTileLayer
-                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-                layer-type="base"
-                name="OpenStreetMap"
-            ></LTileLayer>
-        </LMap>
-    </div>
+    <LayoutComponent class="home">
+        <div class="map">
+            <LMap
+                ref="map"
+                :use-global-leaflet="false"
+                v-model:zoom="zoom"
+                :center="[43.1421256, 20.5084578]"
+            >
+                <LTileLayer
+                    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                    layer-type="base"
+                    name="OpenStreetMap"
+                ></LTileLayer>
+            </LMap>
+        </div>
+    </LayoutComponent>
 </template>
 
 <script setup lang="ts">
+    import { ref } from "vue";
     import { useStore } from "@/stores/store";
     import "leaflet/dist/leaflet.css";
     import { LMap, LTileLayer } from "@vue-leaflet/vue-leaflet";
-    import { ref } from "vue";
 
-    const zoom = ref(200);
+    // Components
+    import LayoutComponent from "@/components/LayoutComponent.vue";
+
     const store = useStore();
+
+    // Data
+    const zoom = ref(200);
+    const map = ref(null);
 </script>
 
 <style scoped lang="scss">
     @import "@/sass/includes";
+
+    .home {
+        .map {
+            height: 100%;
+            width: 100%;
+        }
+    }
 </style>
