ccf6cd8a827e88143523f421e0c45c7702855585
Ramiz Šabović <frostywinterr@gmail.com>
2023-09-12 22:15:19 +0200

    Create a vOnClickOutside vue directive.

---
+++ src/directives/vOnClickOutside.ts
@@ -0,0 +1,15 @@
+const vOnClickOutside = {
+    beforeMount(el, binding) {
+        el.clickOutsideEvent = function (event) {
+            if (!(el == event.target || el.contains(event.target))) {
+                binding.value(event);
+            }
+        };
+        document.body.addEventListener("click", el.clickOutsideEvent);
+    },
+    unmounted(el) {
+        document.body.removeEventListener("click", el.clickOutsideEvent);
+    },
+};
+
+export default vOnClickOutside
