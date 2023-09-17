79440446ba0f4d5a85cdad749dc4a79a5708ebb9
Ramiz Šabović <frostywinterr@gmail.com>
2023-09-17 12:55:27 +0200

    Refactor Zone get and create functions, implement update and delete. Define colors for different zones.

--- src/api/ZoneApi.ts
+++ src/api/ZoneApi.ts
@@ -1,5 +1,19 @@
 import { type AxiosInstance } from "axios";
 
+export enum EZoneColors {
+    ZONE_COLOR = "#FF0000",
+    GARAGE_AREA_COLOR = "#00FF00",
+    UNDERGROUND_AREA_COLOR = "#FFFF00",
+    LOT_AREA_COLOR = "#0000FF",
+}
+
+export interface IZoneData {
+    guid: string;
+    geoJSON: object;
+    name: string;
+    parkingAreas: Array<object>;
+}
+
 export default class ZoneApi {
     axios: AxiosInstance;
 
@@ -7,15 +21,38 @@ export default class ZoneApi {
         this.axios = axios;
     }
 
-    async getZones() {
-        return await this.axios.get("/Zone/All");
+    async all(): Promise<Array<IZoneData>> {
+        const response = await this.axios.get("/Zone/All");
+
+        const zones = response.data;
+
+        zones.forEach((zone: any) => {
+            zone.geoJSON = JSON.parse(zone.geoJSON);
+        });
+
+        return zones;
     }
 
-    async createZone(name: string, geoJson: object, color: string) {
+    async create(name: string, geoJson: object) {
         await this.axios.post("/Zone/Add", {
             Name: name,
-            GeoJSON: geoJson,
-            Color: color,
+            GeoJSON: JSON.stringify(geoJson),
+        });
+    }
+
+    async update(zone: IZoneData) {
+        await this.axios.patch("/Zone/Update", {
+            GUID: zone.guid,
+            Name: zone.name,
+            GeoJSON: JSON.stringify(zone.geoJSON),
+        });
+    }
+
+    async delete(guid: string) {
+        await this.axios.delete("/Zone/Delete", {
+            data: {
+                guid: guid,
+            }
         });
     }
 }
